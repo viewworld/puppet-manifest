3133e4ed449eb054fbee7dcce03c73145d03a8b5
Michael Budde <mbudde@gmail.com>
2013-09-27 15:56:05 +0200

    Add rabbitmq service and conf file

--- modules/rabbitmq/manifests/init.pp
+++ modules/rabbitmq/manifests/init.pp
@@ -4,4 +4,15 @@ class rabbitmq() {
     ensure => installed,
   }
 
+  service { 'rabbitmq-server':
+    ensure => running,
+    enable => true,
+    require => Package['rabbitmq-server']
+  }
+
+  file { '/etc/rabbitmq/rabbitmq-env.conf':
+    content => template('rabbitmq/rabbitmq-env.conf.erb'),
+    notify  => Service['rabbitmq-server']
+  }
+
 }
